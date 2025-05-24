ff161a0c8cd83d7237ac91cf3fb824a939a4fb71
IljaBukhal <tom2.2018@mail.ru>
2025-05-24 14:02:26 +0300

    implemented loading and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,29 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const templateCard = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function addCard(img, ttl) {
+   const cardElement = templateCard.cloneNode(true);
+   const cardImage = cardElement.querySelector('.card__image');
+   const cardTitle = cardElement.querySelector('.card__title');
+   const deleteButton = cardElement.querySelector('.card__delete-button');
+
+   cardImage.setAttribute('src', img);
+   cardImage.setAttribute('alt', 'фотография места');
+   cardTitle.insertAdjacentText('afterbegin', ttl);
+   deleteButton.addEventListener('click', evt => {
+      evt.target.closest('.card').remove();
+   });
+
+   placesList.append(cardElement);
+}
+
+function addAllCards() {
+   initialCards.forEach(card => {
+      addCard(card.link, card.name);
+   });
+}
+
+addAllCards();
