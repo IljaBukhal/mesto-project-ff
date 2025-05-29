1a82b000996681c4817966064e60470d116c60a2
IljaBukhal <tom2.2018@mail.ru>
2025-05-29 13:50:07 +0300

    corrected the assignment of the value to the alt attribute

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ function createCard(card) {
    const deleteButton = cardElement.querySelector('.card__delete-button');
 
    cardImage.setAttribute('src', card.link);
-   cardImage.setAttribute('alt', 'фотография места');
+   cardImage.setAttribute('alt', card.name);
    cardTitle.insertAdjacentText('afterbegin', card.name);
    deleteButton.addEventListener('click', handleDeleteCard);
    return cardElement;
